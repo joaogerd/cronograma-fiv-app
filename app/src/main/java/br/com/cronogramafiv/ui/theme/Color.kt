faf9094cc16dd32676397e2b3d99bc78e8c44fcf
João Gerd Zell de Mattos <joao.gerd@gmail.com>
2026-05-02 10:11:33 -0300

    chore: add Compose theme colors

---
+++ app/src/main/java/br/com/cronogramafiv/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package br.com.cronogramafiv.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryGreen = Color(0xFF2E7D32)
+val OnPrimary = Color(0xFFFFFFFF)
+val Background = Color(0xFFFAFDF7)
+val Surface = Color(0xFFFFFFFF)
+val SurfaceVariant = Color(0xFFEAF3E6)
+val OnSurface = Color(0xFF1B1F1A)
