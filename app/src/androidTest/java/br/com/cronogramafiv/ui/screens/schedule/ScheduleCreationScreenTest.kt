ec52607ad0abe22dc609b1bab0a2d3da5774301d
João Gerd Zell de Mattos <joao.gerd@gmail.com>
2026-05-02 21:27:36 -0300

    test: add MVP UI smoke test

---
+++ app/src/androidTest/java/br/com/cronogramafiv/ui/screens/schedule/ScheduleCreationScreenTest.kt
@@ -0,0 +1,28 @@
+package br.com.cronogramafiv.ui.screens.schedule
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import br.com.cronogramafiv.ui.theme.CronogramaFivTheme
+import org.junit.Rule
+import org.junit.Test
+
+class ScheduleCreationScreenTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun scheduleCreationScreenShowsMainFields() {
+        composeTestRule.setContent {
+            CronogramaFivTheme {
+                ScheduleCreationScreen()
+            }
+        }
+
+        composeTestRule.onNodeWithText("Cronograma FIV").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Protocolo").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Base do cálculo").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Data de referência").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Gerar cronograma").assertIsDisplayed()
+    }
+}
