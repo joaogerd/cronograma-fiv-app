38b9cf1ad4b715b9efc1721eba992dc0d3f72288
João Gerd Zell de Mattos <joao.gerd@gmail.com>
2026-05-05 11:42:44 -0300

    feat: connect main activity to app navigation shell

--- app/src/main/java/br/com/cronogramafiv/MainActivity.kt
+++ app/src/main/java/br/com/cronogramafiv/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import br.com.cronogramafiv.ui.screens.schedule.ScheduleCreationRoute
+import br.com.cronogramafiv.ui.navigation.CronogramaFivApp
 import br.com.cronogramafiv.ui.theme.CronogramaFivTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    ScheduleCreationRoute(
+                    CronogramaFivApp(
                         scheduleRepository = app.scheduleRepository,
                     )
                 }
